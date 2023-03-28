ea89953e1c1ba8addbc9e275e1f0147319a4b77e
Jeremiah Gleason <quickshiftx@gmail.com>
2023-03-27 23:30:31 -0400

    Finished puzzle

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,152 @@
+use std::io;
 
-#[derive(Debug)]
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+
+fn main() {
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let inputs = input_line.split(" ").collect::<Vec<_>>();
+    let r = parse_input!(inputs[0], i8); // number of rows.
+    let c = parse_input!(inputs[1], i8); // number of columns.
+    let a = parse_input!(inputs[2], i8); // number of rounds between the time the alarm countdown is activated and the time the alarm goes off.
+
+    let mut unexplored_moves = Vec::<String>::new();
+    let mut control_room_moves = Vec::<String>::new();
+
+    let mut start_x = -1;
+    let mut start_y = -1;
+
+    let mut c_x = -1;
+    let mut c_y = -1;
+
+    // game loop
+    loop {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let inputs = input_line.split(" ").collect::<Vec<_>>();
+        let kr = parse_input!(inputs[0], i32); // row where Rick is located.
+        let kc = parse_input!(inputs[1], i32); // column where Rick is located.
+        let mut board: Vec<Vec<u8>> = Vec::new();
+        for _ in 0..r as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            // C of the characters in '#.TC?' (i.e. one line of the ASCII maze).
+            board.push(input_line.trim().as_bytes().to_vec());
+        }
+
+        board[kr as usize][kc as usize] = b'K';
+
+        if start_x == -1 && start_y == -1 {
+            start_x = kc;
+            start_y = kr;
+        }
+
+        if control_room_moves.is_empty() {
+
+            let mut next_unexplored_moves = Vec::<String>::new();
+            let mut closest_unexplored = -1;
+            'outer: for (y, vec) in board.iter().enumerate() {
+                for (x, &c) in vec.iter().enumerate() {
+                    if c == b'?' && unexplored_moves.is_empty() {
+                        let mut new_board = board.clone();
+
+                        new_board[y][x] = b'Q';
+
+                        let temp_unexplored_moves = find_shortest_route(
+                            new_board,
+                            kc as i32,
+                            kr as i32,
+                            b'Q'
+                        );
+
+                        if !temp_unexplored_moves.is_empty()
+                            &&
+                            (closest_unexplored < 0
+                                || temp_unexplored_moves.len() < closest_unexplored as usize) {
+
+                            closest_unexplored = temp_unexplored_moves.len() as i32;
+
+                            next_unexplored_moves = temp_unexplored_moves;
+                            next_unexplored_moves.pop();
+                            next_unexplored_moves.reverse();
+                        }
+                    } else if c == b'C' {
+
+                        c_x = x as i32;
+                        c_y = y as i32;
+
+                        let temp_control_room_moves = find_shortest_route(
+                            board.clone(),
+                            start_x,
+                            start_y,
+                            b'C'
+                        );
+
+                        //make sure can get to the start from the control room in the correct num moves
+                        if !temp_control_room_moves.is_empty() && temp_control_room_moves.len() <= a as usize {
+
+                            control_room_moves = find_shortest_route(
+                                board.clone(),
+                                kc as i32,
+                                kr as i32,
+                                b'C'
+                            );
+
+                            control_room_moves.reverse();
+
+                            break 'outer;
+                        }
+                    }
+                }
+            }
+            if unexplored_moves.is_empty() {
+                unexplored_moves = next_unexplored_moves;
+            }
+        }
+
+        let mut ran_control_room_move = false;
+        let next_move = if !control_room_moves.is_empty() {
+            ran_control_room_move = true;
+            control_room_moves.pop().expect("pop up top fail")
+        } else {
+            unexplored_moves.pop().expect("pop up top fail")
+        };
+
+        println!("{next_move}");
+
+        if ran_control_room_move && control_room_moves.is_empty() {
+
+            control_room_moves = find_shortest_route(
+                board.clone(),
+                c_x,
+                c_y,
+                b'T'
+            );
+
+            control_room_moves.reverse();
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
 struct Point {
     x: i32,
     y: i32,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct PointAndPath {
     point: Point,
     moves: Vec<String>,
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Copy, Clone)]
 enum IncrementReturnVal {
     Incremented,
     PathCompleted,
     Failure,
-}
-
-fn main() {
-    let mut board: Vec<Vec<u8>> = Vec::new();
-
-    board.push("??????????????????????????????".as_bytes().to_vec());
-    board.push("#..............???????????????".as_bytes().to_vec());
-    board.push("#.#############???????????????".as_bytes().to_vec());
-    board.push("#.....T........???????????????".as_bytes().to_vec());
-    board.push("#.......................#.#..#".as_bytes().to_vec());
-    board.push("#.#######################.#..#".as_bytes().to_vec());
-    board.push("#.....##......##......#....###".as_bytes().to_vec());
-    board.push("#...####..##..##..##..#..#...#".as_bytes().to_vec());
-    board.push("#.........##......##.....#.C.#".as_bytes().to_vec());
-    board.push("##############################".as_bytes().to_vec());
-
-    let mut point = Point{x: 0, y: 0};
-    for (y, vec) in board.iter().enumerate() {
-        for (x, &c) in vec.iter().enumerate() {
-            if c == b'C' {
-                point.x = x as i32;
-                point.y = y as i32;
-            }
-        }
-    }
-
-    let moves = find_shortest_route(
-        board.clone(),
-        point.x,
-        point.y,
-        b'T'
-    );
-
-    if moves.is_empty() {
-        println!("NO PATH FOUND!");
-    } else {
-        for mov in moves {
-            println!("{mov}");
-        }
-        println!("FINISHED!");
-    }
-
 }
 
 fn find_shortest_route(
@@ -138,7 +227,10 @@ fn increment_if_valid_point(
         || board_copy.is_empty()
         || x < 0
         || board_copy[0].len() <= x as usize
-        || (board_copy[y as usize][x as usize] != b'.' && board_copy[y as usize][x as usize] != *end_char)
+        || (board_copy[y as usize][x as usize] != b'.'
+        && board_copy[y as usize][x as usize] != b'T'
+        && board_copy[y as usize][x as usize] != b'K'
+        && board_copy[y as usize][x as usize] != *end_char)
     {
         IncrementReturnVal::Failure
     } else if board_copy[y as usize][x as usize] == *end_char {
